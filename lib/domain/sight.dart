75ca7f658957e4e440b95d63d2c6550dc32709bf
Kirill Mironov <kirill.mironov@gmail.com>
2020-12-06 11:37:40 +0300

    add Sight class and mocks

---
+++ lib/domain/sight.dart
@@ -0,0 +1,18 @@
+class Sight {
+  final String name;
+  final double lon;
+  final double lat;
+  final String url;
+  final String details;
+  // какой тип у type будет, пока сделал просто String
+  final String type;
+
+  Sight({
+    this.name,
+    this.lon,
+    this.lat,
+    this.url,
+    this.details,
+    this.type,
+  });
+}
