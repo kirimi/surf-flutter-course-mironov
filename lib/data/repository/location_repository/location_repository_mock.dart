9c37fec8428ae6407f276037158c1b5261d34e04
Kirill Mironov <kirill.mironov@gmail.com>
2021-02-01 22:11:27 +0300

    location repository fix

--- lib/data/repository/location_repository/location_repository_mock.dart
+++ lib/data/repository/location_repository/location_repository_mock.dart
@@ -3,12 +3,18 @@ import 'package:places/domain/model/geo_point.dart';
 
 /// Моковые данные для текущего местоположения
 class LocationRepositoryMock implements LocationRepository {
+  GeoPoint _currentLocation;
+
   /// Отдает местоположение с задержкой 500ms
   @override
   Future<GeoPoint> getCurrentLocation() {
-    return Future.delayed(const Duration(milliseconds: 500), () {
-      // todo тут получить реальные координаты
-      return GeoPoint(lon: 59.685994, lat: 30.433278);
-    });
+    if (_currentLocation != null) {
+      return Future.value(_currentLocation);
+    } else {
+      _currentLocation = GeoPoint(lon: 59.685994, lat: 30.433278);
+      return Future.delayed(const Duration(milliseconds: 10), () {
+        return Future.value(_currentLocation);
+      });
+    }
   }
 }
